aa35837f6aca0b9e539a565e1623b6086ffe744a
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-24 15:40:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,10 @@
-def my_each(array)
+def my_each(collection)
   i = 0
-  while i < array.length
+  while i < collection.length
    
-    yield array[i]
+    yield collection[i]
     i += 1
   end
-array
+collection
 end
 
